dc74566b0971878374ea6dfb9dc3d554fdcb198a
Krylova Nataliya <natalinebo2014@yandex.ru>
2023-01-17 12:29:28 +0100

    feat: add css stylesheets

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -25,7 +25,7 @@ export default function WeatherIcon(props) {
   return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
-      color="#1d6efd"
+      color="#c78baf"
       size={props.size}
       animate={true}
     />
